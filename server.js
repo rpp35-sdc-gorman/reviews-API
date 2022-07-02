0987f1d6917ad7430c20a897cc65af3cf94d4408
Josh Waalkes <josh.waalkes21@gmail.com>
2022-07-02 14:32:47 -0600

    outlined api routes

--- server.js
+++ server.js
@@ -6,6 +6,31 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+///// ROUTES /////
+
+app.get('/reviews', (req, res) => {
+  //parse values for query
+  //run db query
+    //success: send results
+    //error: console.log and send results
+});
+
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  //parse review id into query
+  //run db query
+    //success: send success message/status code
+    //error: log and send error
+});
+
+app.post('/reviews', (req, res) => {
+  //parse review id into query
+  //run db query
+    //success: send success message/status code
+    //error: log and send error
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
